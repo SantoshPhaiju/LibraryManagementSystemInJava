8f7ca872b3bc26c2509ac98540432c2ada89bd2f
SantoshPhaiju <santoshphaiju062@gmail.com>
2025-05-06 20:47:34 +0545

    fixed the issue with the code here...

--- src/Service/FileHandlingServices.java
+++ src/Service/FileHandlingServices.java
@@ -1,4 +1,4 @@
-package Services;
+package Service;
 
 import java.io.*;
 
--- src/Service/Logimplement.java
+++ src/Service/Logimplement.java
@@ -1,4 +1,4 @@
-package Services;
+package Service;
 
 public interface Logimplement {
     public void deleteLog(String bookName,int bookid);
