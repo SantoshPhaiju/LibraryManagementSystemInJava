c618da76a2fadf2fe090722079c249aaf0f3558e
SantoshPhaiju <santoshphaiju062@gmail.com>
2025-05-17 22:26:02 +0545

    Adding jdbc connection to the library management

---
+++ src/Database/DatabaseConnection.java
@@ -0,0 +1,18 @@
+package Database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/library";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+
+    public static Connection getConnection() throws ClassNotFoundException, SQLException {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(DB_URL, USER, PASSWORD);
+
+    }
+}
