6c397397db9e1231acf581555e5aeb4cbb46a74f
SantoshPhaiju <santoshphaiju062@gmail.com>
2025-05-18 21:06:39 +0545

    System GUI FRAME CREATED.

--- src/LibraryGUI.java
+++ src/LibraryGUI.java
@@ -1,5 +1,11 @@
+import ui.SystemGUI;
+
+import javax.swing.*;
+
 public class LibraryGUI {
     public static void main(String[] args) {
+        JFrame systemFrame = new SystemGUI();
 
+        systemFrame.setVisible(true);
     }
 }
---
+++ src/ui/SystemGUI.java
@@ -0,0 +1,12 @@
+package ui;
+
+import javax.swing.*;
+
+public class SystemGUI extends JFrame {
+    public SystemGUI() {
+        setTitle("Library Management System");
+        setSize(800, 600);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLayout(null);
+    }
+}
