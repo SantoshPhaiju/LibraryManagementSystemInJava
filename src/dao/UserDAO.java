4e1427c442729bf4a292fd3164c15add11976240
SantoshPhaiju <santoshphaiju062@gmail.com>
2025-06-11 22:19:32 +0545

    fixed error handling with try with resources.

--- src/dao/UserDAO.java
+++ src/dao/UserDAO.java
@@ -2,8 +2,10 @@ package dao;
 
 import entities.User;
 
+import java.sql.SQLException;
+
 public interface UserDAO {
-    public void createUser(User user);
+    public boolean createUser(User user) throws SQLException;
     public void updateUser(User user);
     public void deleteUser(User user);
     public User findUserByEmail(String email);
--- src/dao/UserDaoImpl.java
+++ src/dao/UserDaoImpl.java
@@ -9,23 +9,24 @@ import java.sql.SQLException;
 
 public class UserDaoImpl implements UserDAO {
     @Override
-    public void createUser(User user) {
-        try {
-            Connection connection = DatabaseConnection.getConnection();
+    public boolean createUser(User user) throws SQLException {
+        try (Connection connection = DatabaseConnection.getConnection(); PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO users (username, email) VALUES (?,?)")) {
 
-            PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO users (username, email) VALUES (?,?)");
             preparedStatement.setString(1, user.getUsername());
             preparedStatement.setString(2, user.getEmail());
             int rowsAffected = preparedStatement.executeUpdate();
 
             if (rowsAffected > 0) {
                 System.out.println("User created successfully");
+                return true;
             } else {
                 System.out.println("User creation failed");
+                return false;
             }
         } catch (ClassNotFoundException | SQLException e) {
             System.out.println(e.getMessage());
         }
+        return false;
     }
 
     @Override
