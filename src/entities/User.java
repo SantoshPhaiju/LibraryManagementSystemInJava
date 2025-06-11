47210ddaf3fb8083dc5b4f351753332b802ec794
SantoshPhaiju <santoshphaiju062@gmail.com>
2025-06-11 22:35:28 +0545

    changes in user

--- src/entities/User.java
+++ src/entities/User.java
@@ -1,8 +1,18 @@
 package entities;
 
 public class User {
+    private int id;
     private String username;
     private String email;
+    private String timestamp;
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
 
     public User(String username) {
         this.username = username;
@@ -10,6 +20,14 @@ public class User {
 
     public String getUsername() {
         return username;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public void setUsername(String username) {
