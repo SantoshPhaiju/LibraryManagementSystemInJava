3130ddcf502b4d49b13e4ee669ae076a3ecadfc1
SantoshPhaiju <santoshphaiju062@gmail.com>
2025-06-12 20:50:08 +0545

    updated transactions dao

--- src/dao/TransactionDao.java
+++ src/dao/TransactionDao.java
@@ -1,10 +1,12 @@
 package dao;
 
+import entities.Transactions;
+
 import java.util.List;
 
 public interface TransactionDao {
     public void issueBook(int bookId, int userId, int borrow_period);
     public void returnBook(int bookId, int userId);
 
-    public void showAllIssuedBooksList();
+    public List<Transactions> showAllTransactions();
 }
