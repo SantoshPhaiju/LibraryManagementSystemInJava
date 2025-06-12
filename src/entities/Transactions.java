80b647cd964c20d0f884ab7adee27233d91bd786
SantoshPhaiju <santoshphaiju062@gmail.com>
2025-06-12 22:14:22 +0545

    add the fields for username and bookname to show

--- src/entities/Transactions.java
+++ src/entities/Transactions.java
@@ -7,6 +7,24 @@ public class Transactions {
     private String transactionType;
     private String dueDate;
     private String returnedDate;
+    private String username;
+    private String bookname;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getBookname() {
+        return bookname;
+    }
+
+    public void setBookname(String bookname) {
+        this.bookname = bookname;
+    }
 
     public int getId() {
         return id;
