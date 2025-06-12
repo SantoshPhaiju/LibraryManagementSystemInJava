b691fd1a66f99f7a16340d32e7feed6875eb3bd8
SantoshPhaiju <santoshphaiju062@gmail.com>
2025-06-12 13:54:58 +0545

    added the getallusers abstract method in userdao interface.

--- src/dao/UserDAO.java
+++ src/dao/UserDAO.java
@@ -3,10 +3,12 @@ package dao;
 import entities.User;
 
 import java.sql.SQLException;
+import java.util.List;
 
 public interface UserDAO {
     public boolean createUser(User user) throws SQLException;
     public void updateUser(User user);
     public void deleteUser(User user);
     public User findUserByEmail(String email);
+    public List<User> getAllUsers();
 }
