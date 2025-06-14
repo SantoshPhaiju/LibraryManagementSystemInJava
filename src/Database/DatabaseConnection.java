1b09b123955d141f0bdb466f407dce74e23dc6b5
SantoshPhaiju <santoshphaiju062@gmail.com>
2025-06-14 06:50:56 +0545

    updated connection

--- src/Database/DatabaseConnection.java
+++ src/Database/DatabaseConnection.java
@@ -10,7 +10,19 @@ public class DatabaseConnection {
     private static final String PASSWORD = "@CoderMan99";
 
     public static Connection getConnection() throws ClassNotFoundException, SQLException {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            return DriverManager.getConnection(DB_URL, USER, PASSWORD);
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        return DriverManager.getConnection(DB_URL, USER, PASSWORD);
+    }
+
+    public static void main(String[] args) {
+        try (Connection connection = DatabaseConnection.getConnection();) {
+            if (connection != null) {
+                System.out.println("Connection Established");
+            } else {
+                System.out.println("Connection Not Established");
+            }
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
